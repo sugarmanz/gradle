c81aa8535c6640b06df5450e45825f5a0e600178
Adam Murdoch <adam@gradle.com>
2021-09-03 10:52:21 +1000

    More changes for previous commit.

--- subprojects/resources/src/main/java/org/gradle/internal/resource/transfer/ExternalResourceAccessor.java
+++ subprojects/resources/src/main/java/org/gradle/internal/resource/transfer/ExternalResourceAccessor.java
@@ -17,7 +17,6 @@
 package org.gradle.internal.resource.transfer;
 
 import org.gradle.api.resources.ResourceException;
-import org.gradle.internal.resource.ResourceExceptions;
 import org.gradle.internal.resource.metadata.ExternalResourceMetaData;
 
 import javax.annotation.Nullable;
@@ -41,27 +40,7 @@ public interface ExternalResourceAccessor {
      * @throws ResourceException If the resource may exist, but not could be obtained for some reason.
      */
     @Nullable
-    default <T> T withContent(URI location, boolean revalidate, ContentAndMetadataConsumer<T> action) throws ResourceException {
-        ExternalResourceReadResponse resource = openResource(location, revalidate);
-        if (resource == null) {
-            return null;
-        }
-        try {
-            try {
-                try (InputStream inputStream = resource.openStream()) {
-                    T result = action.consume(resource.getMetaData(), inputStream);
-                    if (result == null) {
-                        throw new IllegalStateException("Action returned null.");
-                    }
-                    return result;
-                }
-            } finally {
-                resource.close();
-            }
-        } catch (IOException e) {
-            throw ResourceExceptions.getFailed(location, e);
-        }
-    }
+    <T> T withContent(URI location, boolean revalidate, ContentAndMetadataAction<T> action) throws ResourceException;
 
     /**
      * Reads the resource at the given location.
@@ -78,43 +57,9 @@ public interface ExternalResourceAccessor {
      * @throws ResourceException If the resource may exist, but not could be obtained for some reason.
      */
     @Nullable
-    default <T> T withContent(URI location, boolean revalidate, ContentConsumer<T> action) throws ResourceException {
-        ExternalResourceReadResponse resource = openResource(location, revalidate);
-        if (resource == null) {
-            return null;
-        }
-        try {
-            try {
-                try (InputStream inputStream = resource.openStream()) {
-                    T result = action.consume(inputStream);
-                    if (result == null) {
-                        throw new IllegalStateException("Action returned null.");
-                    }
-                    return result;
-                }
-            } finally {
-                resource.close();
-            }
-        } catch (IOException e) {
-            throw ResourceExceptions.getFailed(location, e);
-        }
+    default <T> T withContent(URI location, boolean revalidate, ContentAction<T> action) throws ResourceException {
+        return withContent(location, revalidate, (metaData, inputStream) -> action.execute(inputStream));
     }
-
-    /**
-     * Read the resource at the given location.
-     *
-     * If the resource does not exist, this method should return null.
-     *
-     * If the resource may exist but can't be accessed due to some configuration issue, the implementation
-     * must throw an {@link ResourceException} to indicate a fatal condition.
-     *
-     * @param location The address of the resource to obtain
-     * @param revalidate The resource should be revalidated as part of the request
-     * @return The resource if it exists, otherwise null. Caller is responsible for closing the result.
-     * @throws ResourceException If the resource may exist, but not could be obtained for some reason.
-     */
-    @Nullable
-    ExternalResourceReadResponse openResource(URI location, boolean revalidate) throws ResourceException;
 
     /**
      * Obtains only the metadata about the resource.
@@ -132,12 +77,11 @@ public interface ExternalResourceAccessor {
     @Nullable
     ExternalResourceMetaData getMetaData(URI location, boolean revalidate) throws ResourceException;
 
-    interface ContentConsumer<T> {
-        T consume(InputStream inputStream) throws IOException;
+    interface ContentAction<T> {
+        T execute(InputStream inputStream) throws IOException;
     }
 
-    interface ContentAndMetadataConsumer<T> {
-        T consume(ExternalResourceMetaData metaData, InputStream inputStream) throws IOException;
+    interface ContentAndMetadataAction<T> {
+        T execute(ExternalResourceMetaData metaData, InputStream inputStream) throws IOException;
     }
-
 }
